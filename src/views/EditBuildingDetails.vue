16eae40a964e83fa377d3a1328b13dbff980d14c
rlwhuang <lh1677@nyu.edu>
2021-08-13 17:43:55 +0800

    Update EditBuildingDetails.vue

--- src/views/EditBuildingDetails.vue
+++ src/views/EditBuildingDetails.vue
@@ -1,12 +1,12 @@
 <template>
-  <h1 v-if="buildingData">
-    Editing: Building: {{ buildingData.current_name }}
+  <h1 v-if="formBuildingData">
+    Editing: Building: {{ formBuildingData.current_name }}
   </h1>
   <h1 v-else>Loading...</h1>
   <el-row :gutter="20">
     <el-col :span="16"
       ><div class="grid-content">
-        <div v-if="buildingData" id="basicInformation">
+        <div v-if="formBuildingData" id="basicInformation">
           <el-form
             :model="formBuildingData"
             :rules="rules"
@@ -17,26 +17,31 @@
             <el-form-item label="Current name" prop="current_name">
               <el-input v-model="formBuildingData.current_name"></el-input>
             </el-form-item>
+            <p>For Current address, Former names, and English names, list each item in a new line. </p>
+            <el-form-item label="Current address" prop="current_address">
+              <el-input
+                type="textarea"
+                v-model="formBuildingData.current_address" autosize
+              ></el-input>
+            </el-form-item>
 
             <el-form-item label="Former names" prop="former_names">
               <el-input
                 type="textarea"
-                v-model="formBuildingData.former_names"
+                v-model="formBuildingData.former_names" autosize
               ></el-input>
             </el-form-item>
 
             <el-form-item label="English names" prop="english_names">
               <el-input
                 type="textarea"
-                v-model="formBuildingData.english_names"
+                v-model="formBuildingData.english_names" autosize
               ></el-input>
             </el-form-item>
 
             <h3>Geographic Information</h3>
             <el-form-item label="OpenStreetMaps Way number" prop="geo_osm_way">
-              <el-input
-                v-model="formBuildingData.geo_osm_way"
-              ></el-input>
+              <el-input v-model="formBuildingData.geo_osm_way"></el-input>
             </el-form-item>
             <el-form-item>
               <el-button type="primary" @click="submitForm('building_form')"
@@ -45,34 +50,6 @@
               <el-button @click="resetForm('building_form')">Reset</el-button>
             </el-form-item>
           </el-form>
-
-          <!-- <table>
-            <colgroup>
-              <col style="width: 10em" />
-              <col style="width: 16em" />
-            </colgroup>
-            <tr>
-              <td>Current Name</td>
-              <td><input v-model="buildingData.current_name" /></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>
-                <button @click="writeToDb">Save</button><br /><button>
-                  Reset
-                </button>
-              </td>
-            </tr>
-          </table> -->
-          <!--h3>Lists including the building</h3>
-    <div v-if="buildingData" id="listIncluding">
-      <router-link
-        :to="{ name: 'ListDetails', params: { id: d } }"
-        v-for="d in buildingData.designations"
-        :key="d"
-        >{{ d }}</router-link
-      >
-    </div-->
         </div>
       </div></el-col
     >
@@ -91,27 +68,22 @@ export default {
   data() {
     return {
       id: this.$route.params.id,
-      buildingData: null,
-      formBuildingData: null,
+      buildingData: null, // buildingData is data retrieved from Firestore
+      formBuildingData: null, // formBuildingData is the object to serve as the reactive v-model with the form
       uid: null,
       rules: {
         current_name: [
           {
             required: true,
-            message: "Please input building name",
-            trigger: "blur",
-          },
-          {
-            min: 1,
-            max: 240,
-            message: "Length should be 3 to 5",
+            message: "Current building name is required",
             trigger: "blur",
           },
         ],
-        osm_way: [
+        geo_osm_way: [
           {
+            pattern: /^\d+$/, 
             required: false,
-            message: "Please input OSM way",
+            message: "OSM Way must be a number",
             trigger: "blur",
           },
         ],
@@ -119,61 +91,72 @@ export default {
     };
   },
   mounted() {
-    const splitLines = (str) => str.split(/\r?\n/); //convert multiline string to array
-
     this.uid = firebase.auth().currentUser.uid;
     console.log(this.id);
     if (this.id !== "new") {
-      console.log("update");
+      console.log("MODE: update existing building record");
       db.collection("buildings")
         .doc(this.id)
         .get()
         .then((result) => {
           this.buildingData = result.data();
-          //TODO: construct new object for use on Element-UI form.
+          //Construct formBuildingData from data retrieved from firebase
           this.formBuildingData = {
             current_name: this.buildingData.current_name,
             former_names: this.buildingData.former_names.join("\n"),
             current_address: this.buildingData.current_address.join("\n"),
             english_names: this.buildingData.english_names.join("\n"),
-            geo_osm_way: this.buildingData.hasOwnProperty('geographic_info') ? this.buildingData.geographic_info.osm_way : ""
-           };
+            geo_osm_way: this.buildingData.hasOwnProperty("geographic_info")
+              ? this.buildingData.geographic_info.osm_way
+              : "",
+          };
         });
     } else {
       this.formBuildingData = {
         current_name: "",
         current_address: "",
-        former_names: "", 
-        english_names: "", 
-        geo_osm_way: ""
+        former_names: "",
+        english_names: "",
+        geo_osm_way: "",
       };
     }
   },
   methods: {
     writeToDb() {
+      // convert multiline string to array
+      // taken from https://is.gd/hBt8ka
+      const splitLines = (str) => str.split(/\r?\n/);
+
       if (this.id !== "new") {
+        //update existing building
         var building_ref = db.collection("buildings").doc(this.id);
         console.log("write: update");
 
-        // var newBuildingData = {
-        //     "last_edited_by": this.uid,
-        //     "timestamp": firebase.firestore.Timestamp.now(),
-        //     "current_name": this.buildingData.current_name,
-        //     "current_address": JSON.parse(JSON.stringify(this.buildingData.current_address))
-        // };
+        var newBuildingData = {
+          last_edited_by: this.uid,
+          timestamp: firebase.firestore.Timestamp.now(),
+          current_name: this.formBuildingData.current_name,
+          current_address: splitLines(this.formBuildingData.current_address).filter((a) => a),
+          english_names: splitLines(this.formBuildingData.english_names).filter((a) => a),
+          former_names: splitLines(this.formBuildingData.former_names).filter((a) => a), 
+          //the filter gets rid of empty elements in the array
+          //from https://melvingeorge.me/blog/remove-empty-elements-from-array-javascript
+        };
 
-        var newBuildingData = this.buildingData;
+        newBuildingData["geographic_info"] = {
+          osm_way: this.formBuildingData.geo_osm_way,
+        };
 
         console.log(newBuildingData);
 
-        building_ref
-          .set(newBuildingData, { merge: true })
-          .then(() => {
-            console.log("Document successfully written!");
-          })
-          .catch((error) => {
-            console.error("Error writing document: ", error);
-          });
+        // building_ref
+        //   .set(newBuildingData, { merge: true })
+        //   .then(() => {
+        //     console.log("Document successfully written!");
+        //   })
+        //   .catch((error) => {
+        //     console.error("Error writing document: ", error);
+        //   });
       } else {
         console.log(this.buildingData);
       }
@@ -181,12 +164,14 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert("submit!");
+          //   this.$message.success(
+          //     "Submitted. "
+          //   );
+          this.writeToDb();
         } else {
           this.$message.error(
             "Error: Data validation failed. Please check your input. "
           );
-          //console.log("error submit!!");
           return false;
         }
       });
